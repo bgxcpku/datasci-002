cf70eedccc630139329aca079377fe63b6c7bb91
Zhonghua Xi <xizhonghua@gmail.com>
2014-07-19 01:45:06 -0400

    update

--- pa/assignment3/friendships.py
+++ pa/assignment3/friendships.py
@@ -10,11 +10,9 @@ def mapper(record):
     mr.emit_intermediate(b + ' ' + a, 1)
 
 def reducer(key, list_of_values):
-    friends = set(list_of_values)
     a, b = key.split()
     if len(list_of_values)==1:
         mr.emit((a, b))
-
 
 if __name__ == '__main__':
     inputData = open(sys.argv[1])
